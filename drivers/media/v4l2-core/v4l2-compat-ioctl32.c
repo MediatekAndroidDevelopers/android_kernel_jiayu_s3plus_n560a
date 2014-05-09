61ebd510aa2edd73e8d5b70bc50ab6ca9791fe94
Satish Kodishala <skodisha@codeaurora.org>
2014-05-09 11:39:06 +0530

    v4l2-compat-ioctl32: Add support for private buffers
    Add support for copying length and userptr fields from user
    space private buffers to kernel space and vice versa.
    
    Change-Id: Ia7d41aa312544bb0960670af58623b0dc0435a8a
    Signed-off-by: Satish Kodishala <skodisha@codeaurora.org>

--- drivers/media/v4l2-core/v4l2-compat-ioctl32.c
+++ drivers/media/v4l2-core/v4l2-compat-ioctl32.c
@@ -404,6 +404,16 @@ static int get_v4l2_buffer32(struct v4l2_buffer *kp, struct v4l2_buffer32 __user
 					&up->timestamp.tv_usec))
 			return -EFAULT;
 
+	if (V4L2_TYPE_IS_PRIVATE(kp->type)) {
+		compat_long_t tmp;
+
+		if (get_user(kp->length, &up->length) ||
+				get_user(tmp, &up->m.userptr))
+			return -EFAULT;
+
+		kp->m.userptr = (unsigned long)compat_ptr(tmp);
+	}
+
 	if (V4L2_TYPE_IS_MULTIPLANAR(kp->type)) {
 		num_planes = kp->length;
 		if (num_planes == 0) {
@@ -489,6 +499,12 @@ static int put_v4l2_buffer32(struct v4l2_buffer *kp, struct v4l2_buffer32 __user
 		put_user(kp->reserved, &up->reserved) ||
 		put_user(kp->length, &up->length))
 			return -EFAULT;
+
+	if (V4L2_TYPE_IS_PRIVATE(kp->type)) {
+		if (put_user(kp->length, &up->length) ||
+				put_user(kp->m.userptr, &up->m.userptr))
+			return -EFAULT;
+	}
 
 	if (V4L2_TYPE_IS_MULTIPLANAR(kp->type)) {
 		num_planes = kp->length;
