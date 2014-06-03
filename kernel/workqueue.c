746f38549d032873fb03ed3866395099d501193a
Lai Jiangshan <laijs@cn.fujitsu.com>
2014-06-03 15:32:17 +0800

    workqueue: use schedule_timeout_interruptible() instead of open code
    schedule_timeout_interruptible(CREATE_COOLDOWN) is exactly the same as
    the original code.
    
    Signed-off-by: Lai Jiangshan <laijs@cn.fujitsu.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -2028,8 +2028,7 @@ restart:
 		if (!need_to_create_worker(pool))
 			break;
 
-		__set_current_state(TASK_INTERRUPTIBLE);
-		schedule_timeout(CREATE_COOLDOWN);
+		schedule_timeout_interruptible(CREATE_COOLDOWN);
 
 		if (!need_to_create_worker(pool))
 			break;
