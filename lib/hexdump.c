26efb6841ae9730f31278fce0ff4b3f81b3afea5
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2015-02-12 15:02:24 -0800

    hexdump: fix ascii column for the tail of a dump
    In the current implementation we have a floating ascii column in the tail
    of the dump.
    
    For example, for row size equal to 16 the ascii column as in following
    table
    
    group size \ length	8	12	16
    	1		50	50	50
    	2		22	32	42
    	4		20	29	38
    	8		19	-	36
    
    This patch makes it the same independently of amount of bytes dumped.
    
    The change is safe since all current users, which use ASCII part of the
    dump, rely on the group size equal to 1.  The patch doesn't change
    behaviour for such group size (see the table above).
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/hexdump.c
+++ lib/hexdump.c
@@ -124,7 +124,7 @@ void hex_dump_to_buffer(const void *buf, size_t len, int rowsize,
 			lx += scnprintf(linebuf + lx, linebuflen - lx,
 					"%s%16.16llx", j ? " " : "",
 					(unsigned long long)*(ptr8 + j));
-		ascii_column = 17 * ngroups + 2;
+		ascii_column = rowsize * 2 + rowsize / 8 + 2;
 		break;
 	}
 
@@ -135,7 +135,7 @@ void hex_dump_to_buffer(const void *buf, size_t len, int rowsize,
 		for (j = 0; j < ngroups; j++)
 			lx += scnprintf(linebuf + lx, linebuflen - lx,
 					"%s%8.8x", j ? " " : "", *(ptr4 + j));
-		ascii_column = 9 * ngroups + 2;
+		ascii_column = rowsize * 2 + rowsize / 4 + 2;
 		break;
 	}
 
@@ -146,7 +146,7 @@ void hex_dump_to_buffer(const void *buf, size_t len, int rowsize,
 		for (j = 0; j < ngroups; j++)
 			lx += scnprintf(linebuf + lx, linebuflen - lx,
 					"%s%4.4x", j ? " " : "", *(ptr2 + j));
-		ascii_column = 5 * ngroups + 2;
+		ascii_column = rowsize * 2 + rowsize / 2 + 2;
 		break;
 	}
 
