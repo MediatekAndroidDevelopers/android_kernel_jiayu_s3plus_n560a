49c01b28ab1495657cdc22657cc57219d4d8c047
tadd.kao <tadd.kao@mediatek.com>
2015-10-19 17:21:14 +0800

    masp: remove unused filp related functions.
    [Detail] Open file in kernel space is not a secure way.
    
    [Solution] Remove the file operation in kernel since they are not used.
    
    [Feature] Secure Boot
    
    BUG=23460645
    Change-Id: I79bd3f4f29ca1b1b3aa4ca43b8e8d60382341dbc
    Signed-off-by: tadd.kao <tadd.kao@mediatek.com>
    CR-Id: ALPS02363269

--- drivers/misc/mediatek/masp/asf/asf_inc/sec_fsutil_inter.h
+++
@@ -1,9 +0,0 @@
-#ifndef FSUTILS_INTER_H
-#define FSUTILS_INTER_H
-
-/******************************************************************************
- *  EXPORT FUNCTION
- ******************************************************************************/
-unsigned int sec_fs_read_secroimg (char* path, char* buf);
-
-#endif 
--- drivers/misc/mediatek/masp/asf/asf_inc/sec_usif.h
+++
@@ -1,6 +0,0 @@
-#ifndef USIF_H
-#define USIF_H
-
-extern int sec_usif_check(void);
-
-#endif  // USIFUTILS_H_
