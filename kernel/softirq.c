a931405a50e996bf40f627bc422db58d493bcb97
dragonpt <cesar.maximo@gmail.com>
2015-10-20 18:00:25 +0100

    Revert "Revert "Remove GENERIC_HARDIRQ config option""
    This reverts commit 4d1d31d6de27fccda1c74058e79f5f78b99da3a3.

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -765,7 +765,6 @@ int __init __weak early_irq_init(void)
 	return 0;
 }
 
-#ifdef CONFIG_GENERIC_HARDIRQS
 int __init __weak arch_probe_nr_irqs(void)
 {
 	return NR_IRQS_LEGACY;
@@ -775,4 +774,3 @@ int __init __weak arch_early_irq_init(void)
 {
 	return 0;
 }
-#endif
