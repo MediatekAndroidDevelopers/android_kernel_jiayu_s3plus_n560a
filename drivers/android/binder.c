a48fdac785c324fc38817ae850d815f37abe91b5
Arve Hjønnevåg <arve@android.com>
2016-08-12 16:04:28 -0700

    ANDROID: binder: Clear binder and cookie when setting handle in flat binder struct
    Prevents leaking pointers between processes
    
    BUG: 30768347
    Change-Id: Id898076926f658a1b8b27a3ccb848756b36de4ca
    Signed-off-by: Arve Hjønnevåg <arve@android.com>
    Ticket: PORRIDGE-499

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -2910,7 +2910,9 @@ out_err:
 				fp->type = BINDER_TYPE_HANDLE;
 			else
 				fp->type = BINDER_TYPE_WEAK_HANDLE;
+			fp->binder = 0;
 			fp->handle = ref->desc;
+			fp->cookie = 0;
 			binder_inc_ref(ref, fp->type == BINDER_TYPE_HANDLE,
 				       &thread->todo);
 
@@ -2958,7 +2960,9 @@ out_err:
 					return_error = BR_FAILED_REPLY;
 					goto err_binder_get_ref_for_node_failed;
 				}
+				fp->binder = 0;
 				fp->handle = new_ref->desc;
+				fp->cookie = 0;
 				binder_inc_ref(new_ref, fp->type == BINDER_TYPE_HANDLE, NULL);
 				trace_binder_transaction_ref_to_ref(t, ref,
 								    new_ref);
@@ -3017,6 +3021,7 @@ out_err:
 			binder_debug(BINDER_DEBUG_TRANSACTION,
 				     "        fd %d -> %d\n", fp->handle, target_fd);
 			/* TODO: fput? */
+			fp->binder = 0;
 			fp->handle = target_fd;
 #ifdef BINDER_MONITOR
 			e->fd = target_fd;
