568f28ccb87b6cd26cd0c0777696f3f696a1c804
cm <cesar.maximo@gmail.com>
2016-09-12 22:56:55 +0100

    Cpufreq: Alucard Gov Tuned for Jiayu s3

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -410,6 +410,10 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEPLUS)
 extern struct cpufreq_governor cpufreq_gov_interactiveplus;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactiveplus)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
+extern struct cpufreq_governor cpufreq_gov_alucard;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_alucard)
+
 #endif
 
 
