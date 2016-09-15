f4a4fdcd2a89c429a2992d0e23811b8ccb7c2930
cm <cesar.maximo@gmail.com>
2016-09-15 18:05:12 +0100

    cpufreq: Bring up of wheatley Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -413,7 +413,9 @@ extern struct cpufreq_governor cpufreq_gov_interactiveplus;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_alucard)
-
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
+extern struct cpufreq_governor cpufreq_gov_wheatley;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_wheatley)
 #endif
 
 
