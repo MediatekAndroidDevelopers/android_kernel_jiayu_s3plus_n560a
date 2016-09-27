de7bca09f579ab92f0e46148f2dd0278326a49f9
Shaohua Li <shli@fb.com>
2016-09-27 08:42:42 -0700

    lib: clean up put_cpu_var usage
    put_cpu_var takes the percpu data, not the data returned from
    get_cpu_var.
    
    This doesn't change the behavior.
    
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Shaohua Li <shli@fb.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- lib/random32.c
+++ lib/random32.c
@@ -82,7 +82,7 @@ u32 prandom_u32(void)
 	u32 res;
 
 	res = prandom_u32_state(state);
-	put_cpu_var(state);
+	put_cpu_var(net_rand_state);
 
 	return res;
 }
@@ -129,7 +129,7 @@ void prandom_bytes(void *buf, size_t bytes)
 	struct rnd_state *state = &get_cpu_var(net_rand_state);
 
 	prandom_bytes_state(state, buf, bytes);
-	put_cpu_var(state);
+	put_cpu_var(net_rand_state);
 }
 EXPORT_SYMBOL(prandom_bytes);
 
