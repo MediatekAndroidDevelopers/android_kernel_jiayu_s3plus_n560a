6bfe1a725755ce079498756cafe9d00996d2c678
Dmitry Torokhov <dtor@chromium.org>
2016-10-06 15:53:38 -0700

    CHROMIUM: remove Android's cgroup generic permissions checks
    The implementation is utterly broken, resulting in all processes being
    allows to move tasks between sets (as long as they have access to the
    "tasks" attribute), and upstream is heading towards checking only
    capability anyway, so let's get rid of this code.
    
    BUG=b:31790445,chromium:647994
    TEST=Boot android container, examine logcat
    
    Change-Id: I2f780a5992c34e52a8f2d0b3557fc9d490da2779
    Signed-off-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/394967
    Reviewed-by: Ricky Zhou <rickyz@chromium.org>
    Reviewed-by: John Stultz <john.stultz@linaro.org>
    (cherry picked from commit 6895149f8bf0719aa70487e285fa6a8ad3d2692d)
    Reviewed-on: https://chromium-review.googlesource.com/399858
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Signed-off-by: Mister Oyster <oysterized@gmail.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -6811,12 +6811,6 @@ static int mem_cgroup_can_attach(struct cgroup *cgroup,
 	return ret;
 }
 
-static int mem_cgroup_allow_attach(struct cgroup *cgroup,
-				   struct cgroup_taskset *tset)
-{
-	return subsys_cgroup_allow_attach(cgroup, tset);
-}
-
 static void mem_cgroup_cancel_attach(struct cgroup *cgroup,
 				     struct cgroup_taskset *tset)
 {
@@ -6985,11 +6979,6 @@ static int mem_cgroup_can_attach(struct cgroup *cgroup,
 {
 	return 0;
 }
-static int mem_cgroup_allow_attach(struct cgroup *cgroup,
-				   struct cgroup_taskset *tset)
-{
-	return 0;
-}
 static void mem_cgroup_cancel_attach(struct cgroup *cgroup,
 				     struct cgroup_taskset *tset)
 {
@@ -7025,7 +7014,6 @@ struct cgroup_subsys mem_cgroup_subsys = {
 	.can_attach = mem_cgroup_can_attach,
 	.cancel_attach = mem_cgroup_cancel_attach,
 	.attach = mem_cgroup_move_task,
-	.allow_attach = mem_cgroup_allow_attach,
 	.bind = mem_cgroup_bind,
 	.base_cftypes = mem_cgroup_files,
 	.disabled = 1,	/* Disable it for performance workaround */
