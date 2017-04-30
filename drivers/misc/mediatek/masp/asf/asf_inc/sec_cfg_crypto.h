56cadfa2c6fe1c0665327abdc423896794abae5a
fire855 <thefire855@gmail.com>
2017-05-01 00:05:33 +0200

    masp: remove old asf

--- drivers/misc/mediatek/masp/asf/asf_inc/sec_cfg_crypto.h
+++
@@ -1,9 +0,0 @@
-#ifndef SECCFG_CRYPTO_H
-#define SECCFG_CRYPTO_H
-
-/******************************************************************************
- *  EXPORT FUNCTION
- ******************************************************************************/
-extern unsigned int get_seccfg_cipher_len (void);
-
-#endif /* SECCFG_CRYPTO_H */
--- drivers/misc/mediatek/masp/asf/asf_inc/sec_signfmt_def.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SEC_SIGN_FORMAT_DEF_H
-#define _SEC_SIGN_FORMAT_DEF_H
-
-#include <mach/sec_osal.h>  
-
-#endif
-
